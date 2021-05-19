8b08724af64ddbf0432a1cff7be0177df1a4f868
Kathurima Kimathi <kathurima@healthcloud.co.ke>
2021-05-19 22:00:36 +0300

    change fabfile

--- rp-portfolio/fabfile.py
+++ rp-portfolio/fabfile.py
@@ -1,9 +1,5 @@
 from fabric.api import *
 
-# def runserver():
-#    local('python3 manage.py runserver')
-
-
 def deploy():
     # local('sudo docker image build .')
     # local('sudo docker-compose up --build')
